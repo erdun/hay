16b337472a439bdf1f26bf8224cc999a6e700081
erdun <494251936@qq.com>
2017-02-03 23:47:03 +0800

    modify enviroment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,58 @@
 module.exports = function (grunt) {
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    // config
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %>  created by alex  at <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'dest/temp/app.js',
-                dest: 'dest/<%= pkg.name %>.min.js'
-            }
+  // config
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %>  created by alex  at <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'scripts/build/md.js',
+        dest: 'scripts/dist/<%= pkg.name %>.min.js'
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['scripts/src/*.js'],
+        tasks: ['browserify', 'uglify'],
+      },
+      css: {
+        files: ['source/stylus/*.styl'],
+        tasks: ['stylus']
+      }
+    },
+    stylus: {
+      'source/css/index.css': 'source/stylus/*.styl'
+    },
+    browserify: {
+      dist: {
+        files: {
+          'scripts/build/index.js': ['scripts/src/index.js'],
         },
-        watch: {
-            scripts: {
-                files: ['scripts/src/*.js'],
-                tasks: ['babel', 'uglify'],
-            },
-            css: {
-                files: ['source/stylus/*.styl'],
-                tasks: ['stylus']
-            }
+        options: {
+          "transform": [["babelify", { "presets": ["es2015"] }]]
         },
-        stylus: {
-            'source/css/index.css': 'source/stylus/*.styl'
-        },
-        babel: {
-            options: {
-                sourceMap: true,
-                presets: ['babel-preset-es2015']
-            },
-            dist: {
-                files: {
-                    'scripts/build/b.js': 'scripts/src/b.js'
-                }
-            }
-        },
-        browserify: {
-            dist: {
-                files: {
-                    'scripts/build/module.js': ['scripts/src/b.js'],
-                },
-                options: {
-                     "transform": [["babelify", { "presets": ["es2015"] }]]
-                },
-            },
-        },
-        clean: ["source/css/*", "scripts/build/*"],
-    });
+      },
+    },
+    clean: {
+      all: ["source/css/*", "scripts/build/*"],
+      dev: 'scripts/build/*'
+    }
 
-    // load plugs
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-stylus');
-    grunt.loadNpmTasks('grunt-browserify');
+  })
 
-    // mession
-    grunt.registerTask('default', ['babel', 'clean', 'uglify']);
-    grunt.registerTask('develop', ['watch:dev']);
+  // load plugs
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  // mission
+  grunt.registerTask('default', ['browserify', 'uglify', 'stylus']);
+  grunt.registerTask('dev', ['watch']);
 };
 
 
--- scripts/src/a.js
+++ scripts/src/a.js
@@ -3,3 +3,7 @@ export const a = () => {
   console.log('this is a function')
 }
 
+
+const b = () => {
+  console.log('this is b function');
+}
